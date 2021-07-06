5cb522e35d7e2ecbbd17a0eb2cf4b54b043d2e87
hana <hana@HanaHaru.local>
2021-07-07 08:22:44 +0900

    adding login and signup functions

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -12,7 +12,7 @@
 import { ref } from 'vue'
 import useSignup from '../composables/useSignup'
 export default {
-  setup() {
+  setup(props, context) {
     // refs
     const displayName = ref('')
     const email = ref('')
@@ -21,7 +21,9 @@ export default {
     const { error, signup } = useSignup()
     const handleSubmit = async () => {
       await signup(email.value, password.value, displayName.value)
-      console.log('user signed up')
+      if (!error.value) {
+        context.emit('signup')
+      }
     }
     return { displayName, email, password, handleSubmit, error }
   }
